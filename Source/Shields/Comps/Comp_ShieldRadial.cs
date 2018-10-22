46f1073c7caf862d7546ae8ea6a6e459b5467f13
JonnyNova <JonnyNova@users.noreply.github.com>
2018-10-22 15:29:10 -0400

    Calculate exact collision point on shield with a ray. Fixes #1

--- Source/Shields/Comps/Comp_ShieldRadial.cs
+++ Source/Shields/Comps/Comp_ShieldRadial.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using FrontierDevelopments.General;
 using FrontierDevelopments.Shields.CompProperties;
 using FrontierDevelopments.Shields.Windows;
@@ -67,7 +68,7 @@ namespace FrontierDevelopments.Shields.Comps
 
             if (parent.Faction == Faction.OfPlayer)
             {
-                if (Props.minRadius != Props.maxRadius)
+                if (Props.minRadius != Props.maxRadius)        
                 {
                     yield return new Command_Action
                     {
@@ -88,12 +89,58 @@ namespace FrontierDevelopments.Shields.Comps
 
         public Vector3? Collision(Ray ray, float limit)
         {
-            var point = ray.GetPoint(limit);
-            if (Collision(point))
+            var origin = ray.origin;
+            var destination = ray.GetPoint(limit);
+            var circleOrigin = Common.ToVector3(parent.Position);
+
+            var d = destination - origin;
+            var f = origin - circleOrigin;
+            
+            var a = Vector3.Dot(d, d);
+            var b = Vector3.Dot(2*f, d) ;
+            var c = Vector3.Dot(f, f) - _fieldRadius * _fieldRadius;
+            
+            var discriminant = b*b-4*a*c;
+
+            if (discriminant < 0) return null;
+
+            // ray didn't totally miss sphere,
+            // so there is a solution to
+            // the equation.
+            discriminant = (float)Math.Sqrt(discriminant);
+
+            // either solution may be on or off the ray so need to test both
+            // t1 is always the smaller value, because BOTH discriminant and
+            // a are nonnegative.
+            var t1 = (-b - discriminant)/(2*a);
+            var t2 = (-b + discriminant)/(2*a);
+
+            // 3x HIT cases:
+            //          -o->             --|-->  |            |  --|->
+            // Impale(t1 hit,t2 hit), Poke(t1 hit,t2>1), ExitWound(t1<0, t2 hit), 
+
+            // 3x MISS cases:
+            //       ->  o                     o ->              | -> |
+            // FallShort (t1>1,t2>1), Past (t1<0,t2<0), CompletelyInside(t1<0, t2>1)
+
+            if( t1 >= 0 && t1 <= 1 )
             {
-                return ray.origin;
+                // t1 is the intersection, and it's closer than t2
+                // (since t1 uses -b - discriminant)
+                // Impale, Poke
+                return new Vector3(origin.x + t1 * d.x, origin.y + t1 * d.y, origin.z + t1 * d.z);
             }
-            return null;
+
+            // here t1 didn't intersect so we are either started
+            // inside the sphere or completely past it
+            if( t2 >= 0 && t2 <= 1 )
+            {
+                // ExitWound
+                return new Vector3(origin.x + t1 * d.x, origin.y + t1 * d.y, origin.z + t1 * d.z);
+            }
+
+            // no intersection: FallShort, Past, CompletelyInside
+            return null ;
         }
 
         public void Draw()
