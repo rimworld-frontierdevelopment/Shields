7299c4f4b695baf9115f789159efb26f5b647da4
JonnyNova <JonnyNova@users.noreply.github.com>
2018-11-17 21:04:25 -0500

    Balance climate control network usage

--- Source/General/Comps/Comp_ClimateControlHeatsink.cs
+++ Source/General/Comps/Comp_ClimateControlHeatsink.cs
@@ -9,25 +9,40 @@ namespace FrontierDevelopments.General.Comps
     {
         private ThingComp _airFlowConsumer;
         private bool _connected = true;
+        private float _initialFlow;
 
-        private bool HasAirflow => _connected && ((CentralizedClimateControl.CompAirFlowConsumer)_airFlowConsumer).IsActive();
+        private bool HasAirflow => _connected && AirFlowConsumer.IsActive();
+
+        private CentralizedClimateControl.CompAirFlowConsumer AirFlowConsumer =>
+            (CentralizedClimateControl.CompAirFlowConsumer) _airFlowConsumer; 
 
         public override void PostSpawnSetup(bool respawningAfterLoad)
         {
             base.PostSpawnSetup(respawningAfterLoad);
             _airFlowConsumer = parent.TryGetComp<CentralizedClimateControl.CompAirFlowConsumer>();
+            _initialFlow = AirFlowConsumer.Props.baseAirExhaust;
         }
 
         protected override float AmbientTemp()
         {
             return HasAirflow
-                ? ((CentralizedClimateControl.CompAirFlowConsumer)_airFlowConsumer).AirFlowNet.AverageConvertedTemperature
+                ? AirFlowConsumer.AirFlowNet.AverageConvertedTemperature
                 : base.AmbientTemp();
         }
 
         protected override void DissipateHeat(float kilojoules)
         {
-            if(!HasAirflow) base.DissipateHeat(kilojoules);
+            if (HasAirflow)
+            {
+                var delta = Temp - AirFlowConsumer.AirFlowNet.AverageConvertedTemperature;
+                var flowRate = delta * 1000;
+                if (flowRate < _initialFlow) flowRate = _initialFlow;
+                AirFlowConsumer.Props.baseAirExhaust = flowRate;
+            }
+            else
+            {
+                base.DissipateHeat(kilojoules);
+            }
         }
 
         public override void PostExposeData()
