3e9ad55b2422cb9b560a31abc845c18d06cce5ee
JonnyNova <JonnyNova@users.noreply.github.com>
2019-11-06 07:47:26 -0500

    Simplify transpilers for DropCellFinder and Verb

--- Source/Shields/Harmony/Harmony_DropCellFinder.cs
+++ Source/Shields/Harmony/Harmony_DropCellFinder.cs
@@ -1,7 +1,4 @@
-﻿using System.Collections.Generic;
-using System.Reflection;
-using System.Reflection.Emit;
-using FrontierDevelopments.General;
+﻿using FrontierDevelopments.General;
 using Harmony;
 using RimWorld;
 using Verse;
@@ -10,80 +7,22 @@ namespace FrontierDevelopments.Shields.Harmony
 {
     public class Harmony_DropCellFinder
     {
-        private static bool CheckShielded(Map map, IntVec3 c)
+        private static bool CanPhysicallyDropIntoShielded(Map map, IntVec3 c)
         {
-            return map.GetComponent<ShieldManager>().Shielded(PositionUtility.ToVector3(c), false);
+            return !map.GetComponent<ShieldManager>().Shielded(PositionUtility.ToVector3(c), false);
         }
         
         [HarmonyPatch(typeof(DropCellFinder), "CanPhysicallyDropInto")]
         static class Patch_CanPhysicallyDropInto
         {
-            [HarmonyTranspiler]
-            static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions, ILGenerator il)
+            [HarmonyPostfix]
+            private static bool AddShieldCheck(bool __result, IntVec3 c, Map map, bool canRoofPunch)
             {
-                var patchPhase = 0;
-                Label? successLabel = null;
-                var shieldTestLabel = il.DefineLabel();
-
-                foreach(var instruction in instructions)
+                if (__result)
                 {
-                    switch (patchPhase)
-                    {
-                        // find GetRoof
-                        case 0:
-                            
-                            if (instruction.opcode == OpCodes.Call 
-                                && instruction.operand == AccessTools.Method(typeof(GridsUtility), nameof(GridsUtility.GetRoof)))
-                            {
-                                patchPhase = 1;
-                            }
-                            break;
-                        // find the success label, replace it with shield test
-                        case 1:
-                            if (instruction.opcode == OpCodes.Brfalse)
-                            {
-                                successLabel = instruction.operand as Label?;
-                                instruction.operand = shieldTestLabel;
-                                patchPhase = 2;
-                            }
-                            break;
-                        // find thick roof result
-                        case 2:
-                            if(instruction.opcode == OpCodes.Ldfld 
-                               && instruction.operand as FieldInfo == AccessTools.Field(typeof(RoofDef), nameof(RoofDef.isThickRoof)))
-                            {
-                                patchPhase = 3;
-                            }
-                            break;
-                        // replace the label
-                        case 3:
-                            if (instruction.opcode == OpCodes.Brfalse)
-                            {
-                                instruction.operand = shieldTestLabel;
-                                patchPhase = 4;
-                            }
-                            break;
-                        // find original return true reference
-                        case 4:
-                            if (instruction.opcode == OpCodes.Ldc_I4_0)
-                            {
-                                yield return new CodeInstruction(OpCodes.Ldarg_1)
-                                {
-                                    labels = new List<Label>(new Label[] { shieldTestLabel })
-                                };
-                                yield return new CodeInstruction(OpCodes.Ldarg_0);
-                                yield return new CodeInstruction(OpCodes.Call, AccessTools.Method(typeof(Harmony_DropCellFinder), nameof(CheckShielded)));
-                                yield return new CodeInstruction(OpCodes.Brfalse, successLabel);
-                                yield return new CodeInstruction(OpCodes.Ldc_I4_0);
-                                yield return new CodeInstruction(OpCodes.Ret);
-
-                                // done patching
-                                patchPhase = -1;
-                            }
-                            break;
-                    }
-                    yield return instruction;
+                    return CanPhysicallyDropIntoShielded(map, c);
                 }
+                return false;
             }
         }
     }
--- Source/Shields/Harmony/Harmony_Verb.cs
+++ Source/Shields/Harmony/Harmony_Verb.cs
@@ -24,44 +24,20 @@ namespace FrontierDevelopments.Shields.Harmony
             return caster.Map.GetComponent<ShieldManager>().Shielded(PositionUtility.ToVector3(source), PositionUtility.ToVector3(target.Cell), caster.Faction);
         }
 
-        [HarmonyPatch(typeof(Verb), nameof(Verb.CanHitTargetFrom) , new [] { typeof(IntVec3), typeof(LocalTargetInfo) })]
-        static class Patch_CanHitTargetFrom
+        [HarmonyPatch(typeof(Verb), nameof(Verb.TryFindShootLineFromTo))]
+        static class Patch_Verb_TryFindShootLineFromTo
         {
-            [HarmonyTranspiler]
-            static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions, ILGenerator il)
+            [HarmonyPrefix]
+            static bool AddShieldCheck(ref bool __result, Verb __instance, IntVec3 root, LocalTargetInfo targ, ref ShootLine resultingLine)
             {
-                var patchPhase = 0;
-            
-                foreach (var instruction in instructions)
+                if (ShieldBlocks(__instance.caster, __instance, root, targ))
                 {
-                    switch (patchPhase)
-                    {
-                        // find end of loop
-                        case 0:
-                            if (instruction.opcode == OpCodes.Blt)
-                            {
-                                patchPhase = 1;
-                            }
-                            break;
-                        case 1:
-                            var continueLabel = il.DefineLabel();
-                            instruction.labels.Add(continueLabel);
-                    
-                            yield return new CodeInstruction(OpCodes.Ldarg_0);
-                            yield return new CodeInstruction(OpCodes.Ldfld, AccessTools.Field(typeof(Verb), nameof(Verb.caster)));
-                            yield return new CodeInstruction(OpCodes.Ldarg_0);
-                            yield return new CodeInstruction(OpCodes.Ldarg_1);
-                            yield return new CodeInstruction(OpCodes.Ldarg_2);
-                            yield return new CodeInstruction(OpCodes.Call, AccessTools.Method(typeof(Harmony_Verb), nameof(ShieldBlocks)));
-                            yield return new CodeInstruction(OpCodes.Brfalse, continueLabel);
-                            yield return new CodeInstruction(OpCodes.Ldc_I4_0);
-                            yield return new CodeInstruction(OpCodes.Ret);
-
-                            patchPhase = -1;
-                            break;
-                    }
-                    yield return instruction;
+                    __result = false;
+                    resultingLine = new ShootLine();
+                    return false;
                 }
+
+                return true;
             }
         }
     }
