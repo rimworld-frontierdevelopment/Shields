4a83fcf33180ea6e1949f36083b7946c95d4ece5
JonnyNova <JonnyNova@users.noreply.github.com>
2020-10-03 17:01:30 -0400

    Move circle collision functions to Core

--- Source/Common/Shields/Comps/Comp_ShieldRadial.cs
+++ Source/Common/Shields/Comps/Comp_ShieldRadial.cs
@@ -184,7 +184,7 @@ namespace FrontierDevelopments.Shields.Comps
 
         public bool Collision(Vector3 vector)
         {
-            return Vector3.Distance(PositionUtility.ToVector3(ExactPosition), vector) < Radius;
+            return CollisionUtility.Circle.Point(PositionUtility.ToVector3(ExactPosition), Radius, vector);
         }
 
         public Vector3? Collision(Ray ray, float limit)
@@ -195,81 +195,8 @@ namespace FrontierDevelopments.Shields.Comps
         public Vector3? Collision(Vector3 origin, Vector3 destination)
         {
             if (Mod.Settings.EnableShootingOut && Collision(origin))
-            {
                 return null;
-            }
-
-            var circleOrigin = PositionUtility.ToVector3(ExactPosition);
-
-            var radius = Radius;
-            
-            var d = destination - origin;
-            var f = origin - circleOrigin;
-            
-            var a = Vector3.Dot(d, d);
-            var b = Vector3.Dot(2*f, d) ;
-            var c = Vector3.Dot(f, f) - radius * radius;
-            
-            var discriminant = b*b-4*a*c;
-
-            if (discriminant < 0) return null;
-
-            // ray didn't totally miss sphere,
-            // so there is a solution to
-            // the equation.
-            discriminant = (float)Math.Sqrt(discriminant);
-
-            // either solution may be on or off the ray so need to test both
-            // t1 is always the smaller value, because BOTH discriminant and
-            // a are nonnegative.
-            var t1 = (-b - discriminant)/(2*a);
-            var t2 = (-b + discriminant)/(2*a);
-
-            // 3x HIT cases:
-            //          -o->             --|-->  |            |  --|->
-            // Impale(t1 hit,t2 hit), Poke(t1 hit,t2>1), ExitWound(t1<0, t2 hit), 
-
-            // 3x MISS cases:
-            //       ->  o                     o ->              | -> |
-            // FallShort (t1>1,t2>1), Past (t1<0,t2<0), CompletelyInside(t1<0, t2>1)
-
-            if( t1 >= 0 && t1 <= 1 )
-            {
-                // t1 is the intersection, and it's closer than t2
-                // (since t1 uses -b - discriminant)
-                // Impale, Poke
-                return new Vector3(origin.x + t1 * d.x, origin.y + t1 * d.y, origin.z + t1 * d.z);
-            }
-
-            // here t1 didn't intersect so we are either started
-            // inside the sphere or completely past it
-            if( t2 >= 0 && t2 <= 1 )
-            {
-                // ExitWound
-                return new Vector3(origin.x + t1 * d.x, origin.y + t1 * d.y, origin.z + t1 * d.z);
-            }
-
-            // no intersection: FallShort, Past, CompletelyInside
-            return null;
-        }
-
-        private bool Collides(CellRect rect)
-        {
-            var position = parent.Position;
-            if (rect.minX <= position.x
-                && position.x <= rect.maxX
-                && rect.minZ <= position.z
-                && position.z <= rect.maxZ) return true;
-
-            var a = new Vector3(rect.minX + 0.5f, 0, rect.minZ + 0.5f);
-            var b = new Vector3(rect.minX + 0.5f, 0, rect.maxZ + 0.5f);
-            var c = new Vector3(rect.maxX + 0.5f, 0, rect.maxZ + 0.5f);
-            var d = new Vector3(rect.maxX + 0.5f, 0, rect.minZ + 0.5f);
-
-            return Collision(a, b) != null
-                         || Collision(b, c) != null
-                         || Collision(c, d) != null
-                         || Collision(d, a) != null;
+            return CollisionUtility.Circle.LineSegment(PositionUtility.ToVector3(ExactPosition), Radius, origin, destination);
         }
 
         private bool ShouldDraw(CellRect cameraRect)
@@ -278,7 +205,7 @@ namespace FrontierDevelopments.Shields.Comps
 
             _cameraLast = cameraRect;
 
-            var collides = Collides(cameraRect);
+            var collides = CollisionUtility.Circle.CellRect(ExactPosition, Radius, cameraRect);
             _renderLast = collides;
             return collides;
         }
